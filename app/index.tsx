4c25df6b8476791a6429b38a3e5f3e4b5b796cd2
Bilal Aksoy <sloeyeleet@gmail.com>
2024-07-07 22:45:08 +0300

    Created initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,46 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
-export default function HomeScreen() {
+import beachImage from '@/assets/meditation-images/beach.webp'
+import CustomButton from '@/components/CustomButton'
+import { useRouter } from 'expo-router'
+import AppGradient from '@/components/AppGradient'
+
+const App = () => {
+  const router = useRouter()
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Hello World!</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton
+                title="Get Started"
+                onPress={() => router.push('/nature-meditate')}
+              />
+            </View>
+
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </AppGradient>
+      </ImageBackground>
     </View>
   )
 }
+
+export default App
